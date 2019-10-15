7699d6c693b860e9b1d90217f6d1ce4d3d858c31
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-15 12:11:07 -0400

    debug

--- code/threads/ioAlarm.cc
+++ code/threads/ioAlarm.cc
@@ -5,8 +5,8 @@
 #include "main.h"
 
 ioAlarm:: ioAlarm(){
-    //ioTimer *iotimer =new ioTimer(this);
     ioHandler *ihandler =new ioHandler();
+    ioTimer *iotimer =new ioTimer(this);
 }
 
 void ioAlarm :: CallBack(){
