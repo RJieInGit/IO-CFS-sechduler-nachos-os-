2d5459939abbef60a461c2cf085f89e6118f74c7
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-15 15:18:09 -0400

    debug

--- code/threads/ioAlarm.cc
+++ code/threads/ioAlarm.cc
@@ -17,7 +17,7 @@ void ioAlarm :: CallBack(){
     //wake up the blocked parent thread
     printf("try wake up all\n");
     List<ioRequest*> temp;
-    printf("io in queue %d \n",kernel->ioEventQueue->NumInList());
+    
     while(!kernel->ioEventQueue->IsEmpty()){
         ioRequest *req=kernel->ioEventQueue->RemoveFront();
         if(req->pendingTick<=kernel->stats->totalTicks){
@@ -27,6 +27,7 @@ void ioAlarm :: CallBack(){
         }
         else
         {
+            printf("io in queue %d \n",kernel->ioEventQueue->NumInList());
             temp.Append(req);
         }
     }
