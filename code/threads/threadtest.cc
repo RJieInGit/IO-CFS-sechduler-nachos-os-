7fbc9271550d34ef78143958a4e700c0cda2ad34
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-15 13:33:21 -0400

    debug

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -15,31 +15,21 @@ SimpleThread(int which)
 }
 
 
-void TestIO(){
+void IObound(){
     printf("here1\n");
 Thread *t = new Thread("thread0");
-    t->Fork((VoidFunctionPtr) io::iowrite, (void *)0);
-    printf("here2\n");
-    Thread *t1 = new Thread("thread1");
-    t1->Fork((VoidFunctionPtr) io::iowrite, (void *)0);
-    Thread *t2 = new Thread("thread2");
-    t2->Fork((VoidFunctionPtr) io::iowrite, (void *)0);
-    Thread *t3 = new Thread("thread3");
-    t3->Fork((VoidFunctionPtr) io::ioread, (void *) 0);
-    Thread *t4 = new Thread("thread4");
-    printf("here3\n");
-    t4->Fork((VoidFunctionPtr) io::ioread, (void *) 0);
-    Thread *t5 = new Thread("thread5");
-    t5->Fork((VoidFunctionPtr) io::iowrite, (void *) 0);
-    Thread *t6 = new Thread("thread6");
-    t6->Fork((VoidFunctionPtr) io::ioread, (void *) 0);
+   io::iowrite(0);
+   io::iowrite(1);
+   io::ioread(0);
+   io::iowrite(2);
+   io::ioread(1);
     
 }
 void
 ThreadTest()
 {
     printf("here0\n");
-    TestIO();
+    IObound();
     
 }
 
