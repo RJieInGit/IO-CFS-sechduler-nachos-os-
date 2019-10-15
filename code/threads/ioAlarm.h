f185b9ddf6b2cec970aee0ab200c7a7c2cfe8846
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-15 11:39:38 -0400

    debug

--- code/threads/ioAlarm.h
+++ code/threads/ioAlarm.h
@@ -1,6 +1,7 @@
 #include "../machine/callback.h"
 #include "../machine/iotimer.h"
 #include "../lib/utility.h"
+#pragma once
 class ioAlarm : public CallBackObj{
     public :
         ioAlarm();    //we get random pending tiem from iorequest
