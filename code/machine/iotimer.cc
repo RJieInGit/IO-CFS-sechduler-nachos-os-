cefa2bcf42d5a852ea256a775bddbcba0584cee3
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-15 10:49:06 -0400

    debug

--- code/machine/iotimer.cc
+++ code/machine/iotimer.cc
@@ -8,8 +8,7 @@
 //initialize a ioTimer and set the interrupt if the eventQueue is not empty
  ioTimer:: ioTimer(CallBackObj *toCall){
     ioCallBack =toCall;
-    ihandler = tocall->ihandler;
-    ioRequest *req = ihandler->getNextInterrupt();
+    ioRequest *req = tocall->ihandler->getNextInterrupt();
     if(req !=NULL){
         SetInterrupt(req);
     }
