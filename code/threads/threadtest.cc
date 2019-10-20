f0b1eb7613378d291e048ca9d388de9f5e729819
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-20 17:44:17 -0400

    debug

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -54,17 +54,20 @@ ThreadTest()
     int numIO =7;
     int numMix =7;
     Thread *t;
-    char s[50];
+    
     for (int i=0;i<numCPU;i++){
+        char s [50];
          sprintf(s,"CPU bound %d",i);
         t= new Thread(s);
         t->Fork((VoidFunctionPtr) CPUbound, (void *) 1);
     }
      for (int i=0;i<numIO;i++){
+         char s [50];
         sprintf(s,"IO bound %d",i);
         t= new Thread(s);
         t->Fork((VoidFunctionPtr) IObound, (void *) 1);
     } for (int i=0;i<numMix;i++){
+        char s [50];
         sprintf(s,"MIX bound %d",i);
         t= new Thread(s);
         t->Fork((VoidFunctionPtr) MIXbound, (void *) 1);
