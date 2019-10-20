da704e867af3f18dc515e7fc374a8ad027f277ab
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-20 14:23:58 -0400

    debug

--- code/threads/scheduler.cc
+++ code/threads/scheduler.cc
@@ -111,7 +111,7 @@ Scheduler::FindNextToRun ()
 void
 Scheduler::Run (Thread *nextThread, bool finishing)
 {
-    readyList->inOrder();
+   // readyList->inOrder();
     // vrt= vrt + runtime*decay
     nextThread->vruntime+= 1000*nextThread->decay/(readyList->getNum()+1);
     //set the time slice for this thread according to its decay.
