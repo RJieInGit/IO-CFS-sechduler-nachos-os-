afa9207f990dc9246088f1fcfca94a671aba3f9c
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-10-20 20:02:56 -0400

    debug

--- code/lib/rbtree.h
+++ code/lib/rbtree.h
@@ -1,6 +1,5 @@
 /**
- * C++ 语言: 红黑树
- *
+ *https://www.cnblogs.com/skywang12345/p/3624291.html
  * @author skywang
  * @date 2013/11/07
  */
@@ -30,7 +29,7 @@ class RBTNode{
 template <class T>
 class RBTree {
     private:
-        RBTNode<T> *mRoot;    // 根结点
+        RBTNode<T> *mRoot;    
 
     public:
         RBTree(int (*comp)(T x, T y));
@@ -47,27 +46,23 @@ class RBTree {
       
         RBTNode<T>* iterativeSearch(T key);
 
-        // 查找最小结点：返回最小结点的键值。
+       
         T minimum();
-        // 查找最大结点：返回最大结点的键值。
+
         T maximum();
 
-        // 找结点(x)的后继结点。即，查找"红黑树中数据值大于该结点"的"最小结点"。
         RBTNode<T>* successor(RBTNode<T> *x);
-        // 找结点(x)的前驱结点。即，查找"红黑树中数据值小于该结点"的"最大结点"。
+ 
         RBTNode<T>* predecessor(RBTNode<T> *x);
 
-        // 将结点(key为节点键值)插入到红黑树中
         void insert(T key);
 
-        // 删除结点(key为节点键值)
         void remove(T key);
         
         T removeMin();
-        // 销毁红黑树
+
         void destroy();
 
-        // 打印红黑树
         void print();
 
         int getNum(){return num;};
@@ -75,40 +70,35 @@ class RBTree {
         int num;      
          
         int (*compare)(T x, T y);
-        // 前序遍历"红黑树"
+     
         void preOrder(RBTNode<T>* tree) const;
-        // 中序遍历"红黑树"
+    
         void inOrder(RBTNode<T>* tree) const;
-        // 后序遍历"红黑树"
+ 
         void postOrder(RBTNode<T>* tree) const;
 
-        // (递归实现)查找"红黑树x"中键值为key的节点
         RBTNode<T>* search(RBTNode<T>* x, T key) const;
-        // (非递归实现)查找"红黑树x"中键值为key的节点
+     
         RBTNode<T>* iterativeSearch(RBTNode<T>* x, T key) const;
 
-        // 查找最小结点：返回tree为根结点的红黑树的最小结点。
         RBTNode<T>* minimum(RBTNode<T>* tree);
-        // 查找最大结点：返回tree为根结点的红黑树的最大结点。
+
         RBTNode<T>* maximum(RBTNode<T>* tree);
 
-        // 左旋
         void leftRotate(RBTNode<T>* &root, RBTNode<T>* x);
-        // 右旋
+    
         void rightRotate(RBTNode<T>* &root, RBTNode<T>* y);
-        // 插入函数
+
         void insert(RBTNode<T>* &root, RBTNode<T>* node);
-        // 插入修正函数
+
         void insertFixUp(RBTNode<T>* &root, RBTNode<T>* node);
-        // 删除函数
+ 
         void remove(RBTNode<T>* &root, RBTNode<T> *node);
-        // 删除修正函数
+ 
         void removeFixUp(RBTNode<T>* &root, RBTNode<T> *node, RBTNode<T> *parent);
 
-        // 销毁红黑树
         void destroy(RBTNode<T>* &tree);
 
-        // 打印红黑树
         void print(RBTNode<T>* tree, T key, int direction);
 
 #define rb_parent(r)   ((r)->parent)
